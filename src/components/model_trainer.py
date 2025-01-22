e8c89e382703cdb1b251c07dd88f54b34dc59379
Ajay Shanbhag <ajayshan2004@gmail.com>
2025-01-22 18:43:22 +0530

    Fixing model training bugs

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -30,12 +30,48 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "KNN Regression": KNeighborsRegressor(),
-                "Catboost Regression": CatBoostRegressor(),
+                "XGB Regression": XGBRegressor(),
+                "CatBoost Regression": CatBoostRegressor(),
                 "AdaBoost Regression": AdaBoostRegressor()
             }
 
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.01,.05,.001],
+                    'subsample':[0.8,0.85,0.9],
+                    'n_estimators': [64,128,256]
+                },
+                "Linear Regression":{},
+                "XGB Regression":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [32,64,128,256]
+                },
+                "CatBoost Regression":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regression":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators': [32,64,128,256]
+                }
+                
+            }
+
+            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
 
             best_model_score = max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -16,15 +17,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for i in range(len(list(models))):
              model = list(models.values())[i]
-             model.fit(X_train, y_train)
+             print('Trying model {0}'.format(list(models.keys())[i]))
+             param_list=params[list(models.keys())[i]]
+             gcv = GridSearchCV(model,param_list,cv=3)
+             gcv.fit(X_train, y_train)
+             model.set_params(**gcv.best_params_)
+             model.fit(X_train,y_train)
              y_train_pred = model.predict(X_train)
              y_test_pred = model.predict(X_test)
-             train_model_score = r2_score(y_train, y_train_pred)
              test_model_score = r2_score(y_test, y_test_pred)
              report[list(models.keys())[i]] = test_model_score
         return report
